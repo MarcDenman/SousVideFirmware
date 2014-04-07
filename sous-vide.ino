17d08ff352b85e7d5b09027b6c1a55a7810ac296
Marc Denman <marc@marcdenman.co.uk>
2014-04-07 23:11:11 +0100

    Initial Working Code

--- sous-vide.ino
+++ sous-vide.ino
@@ -22,49 +22,93 @@ static DS1820Sensor sensor;
 // Global Definitions
 // ***********************************
 char cTemp[10];                         //Convert float to String for posting 
-
+volatile long onTime = 0;
 
 // ***********************************
 // PID Variables and constants
 // ***********************************
 
 //Define Variables we'll be connecting to
-double Setpoint;
+double Setpoint = 50;
 double Input;
 double Output;
 
 // pid tuning parameters
-double Kp;                              //
-double Ki;                              //
-double Kd;                              //
+double Kp = 0.1;                              //http://www.over-engineered.com/projects/sous-vide-pid-controller/
+double Ki = 150;                              //
+double Kd = 0.45;                             //
 
 // 10 second Time Proportional Output window
 int WindowSize = 10000; 
 unsigned long windowStartTime;
+unsigned long now;
 
 //Specify the links and initial tuning parameters
+//Input = Current Temp of the SousVide machine 
+//Output = ?
+//Setpoint = Target Temp
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 
 // This routine runs only once upon reset
 void setup() {
+    
+    windowStartTime = millis();
 
     pinMode(RELAY_PIN, OUTPUT);
-    digitalWrite(RELAY_PIN, LOW);       //Ensure the relay is turned off to begin with.
+    digitalWrite(RELAY_PIN, HIGH);       //Ensure the relay is turned off to begin with.
+    
 	Serial.begin(9600);
 	delay(1000);
-
+	
+	myPID.SetTunings(Kp, Ki, Kd);
+	
+	myPID.SetSampleTime(1000);
+	myPID.SetOutputLimits(0, WindowSize);
+	
+	myPID.SetMode(AUTOMATIC);
 }
 // This routine gets called repeatedly, like once every 5-15 milliseconds.
 // Spark firmware interleaves background CPU activity associated with WiFi + Cloud activity with your code. 
 // Make sure none of your code delays or blocks for too long (like more than 5 seconds), or weird things can happen.
 void loop() {
+    
+    //windowStartTime = millis();
+    
+    //windowStartTime = millis();
     updateSensor();
-    
+    Input = sensor.GetTemp()/1000000;
 	delay(1000);  // Wait for 1 second in off mode
-    getTemp();
+    //getTemp();
+    myPID.Compute();
+    now = millis();
     
+    
+     if(now - windowStartTime>WindowSize)
+    { //time to shift the Relay Window
+        windowStartTime += WindowSize;
+        Serial.println("New WIndow");
+    }
+
+    //double test = (double)(now - windowStartTime);
+     if(Output > now - windowStartTime)
+     {
+         digitalWrite(RELAY_PIN,HIGH);
+     }
+    else 
+    {
+        digitalWrite(RELAY_PIN,LOW);
+        Serial.println(now - windowStartTime);
+        Serial.println((String)Output + "O");
+    }
+    
+    //Serial.println(windowStartTime);
+    //Serial.println((String)Input + "I");
+    //Serial.println((String)Output + "O");
+    //Serial.println(now);
     delay(1000);
+    //Serial.println((String)windowStartTime + "W");*/
+    
 }           
 
 
@@ -116,38 +160,3 @@ int updateSensor()
             sprintf(cTemp, "%ld", tempd);
             Serial.println(cTemp);
  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
